11f54dac9a8eb4c20bfb8da28ebd54b3198bb200
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2018-11-29 14:13:21 +0100

    Package fixes

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,12 +1,16 @@
 import React from "react"
+import { HashRouter as Router, Route, Link } from "react-router-dom"
+import StartPage from "../pages/startpage/startpage.js"
 
 class App extends React.Component {
 
   render() {
     return (
-      <div>
-        Find me in src/app.js!
-      </div>
+      <Router>
+        <div>
+          <Route path="/" exact="true" component={StartPage} />
+        </div>
+      </Router>
     )
   }
 
---
+++ frontend/src/pages/startpage/startpage.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { HashRouter as Router, Route, Link } from "react-router-dom"
+import "./startpage.scss"
+
+class StartPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>Hej</p>
+      </div>
+
+    )
+  }
+
+}
+
+export default StartPage
