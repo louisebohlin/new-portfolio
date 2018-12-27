87df141c1ecf933ecb1840df12b216bcda2e9e73
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2018-12-27 16:02:37 +0100

    Contact page done and some minor changes

--- frontend/src/pages/contactpage/contactpage.js
+++ frontend/src/pages/contactpage/contactpage.js
@@ -10,8 +10,24 @@ class ContactPage extends React.Component {
     return (
       <div>
         <Header />
-        <Footer />
-        
+        <div className="contact-wrapper">
+          <div className="contact-info">
+            <ul>
+              <li><a href="mailto:louisebp@gmail.com" target="_blank" >louisebp@gmail.com</a></li>
+              <li>+46 707425010</li>
+            </ul>
+            <ul>
+              <li>Skånegatan 80</li>
+              <li>116 37 Stockholm</li>
+            </ul>
+            <ul>
+              <li><a href="https://www.linkedin.com/in/louise-b%C3%B6hlin-pihlsg%C3%A5rd-96687010b/" target="_blank">Linkedin</a></li>
+              <li><a href="https://www.instagram.com/louisebohlin/" target="_blank">Instagram</a></li>
+              <li><a href="https://www.facebook.com/louisebp" target="_blank">Facebook</a></li>
+            </ul>
+          </div>
+          <Footer />
+        </div>
       </div>
 
     )
