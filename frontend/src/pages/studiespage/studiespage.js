d04d8c00c541f6b77a0c36601b608bec3e23e84b
Louise Böhlin Pihlsgård <louisebp@gmail.com>
2019-01-21 13:48:58 +0100

    Started json fetch, not working yet

--- frontend/src/pages/studiespage/studiespage.js
+++ frontend/src/pages/studiespage/studiespage.js
@@ -2,26 +2,30 @@ import React from "react"
 import { HashRouter as Router, Route, Link } from "react-router-dom"
 import Header from "../../components/header/header.js"
 import Footer from "../../components/footer/footer.js"
+import studiesJson from "./studies.json"
 import "./studiespage.scss"
 
 class StudiesPage extends React.Component {
 
   render() {
+    const id = this.props.match.params.id
+    const study = studiesJson.study[id - 1]
+
     return (
       <div>
         <Header />
-          <div className="studies-wrapper">
-            <div className="study-box">
-              <div className="study-image">
-                <img src="./images/logo-circle.png" />
-              </div>
+        <div className="studies-wrapper">
+          <div className="study-box">
+            <div className="study-image">
+              <img src={study.image}/>
+            </div>
             <div className="study-info">
-              <p>Technigo</p>
-              </div>
+              <h2>{study.name}</h2>
+              <p>{study.description}</p>
             </div>
           </div>
+        </div>
         <Footer />
-
       </div>
 
     )
